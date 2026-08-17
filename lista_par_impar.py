1e0d103b976100b5bde348d0050fe2929f24cddd
ltwendellopes <ltwendellopes@users.noreply.github.com>
2026-08-17 14:06:41 -0300

    Corrige separacao de pares e impares para usar listas de verdade
    Dois problemas no mesmo trecho:
    - A condicao testava a paridade do indice do loop (i % 2), nao a do
      numero digitado (num % 2) - o script nao classificava par/impar
      pelo valor real digitado.
    - par/impar eram variaveis unicas sobrescritas a cada iteracao, entao
      so o ultimo valor de cada categoria sobrevivia ate o print final -
      apesar do nome do repositorio ("por listas"). Se todos os numeros
      digitados fossem pares (ou todos impares), a variavel nao usada
      ficava indefinida e o print final quebrava com NameError.
    
    Agora pares/impares sao listas acumuladas com base no valor de num.

--- lista_par_impar.py
+++ lista_par_impar.py
@@ -1,13 +1,15 @@
 numeros = []
-
+pares = []
+impares = []
 
 for i in range(1, 8):
     num = int(input(f'Digite o {i}º número: '))
     numeros.append(num)
-    if i % 2 == 0:
-        par = num
+    if num % 2 == 0:
+        pares.append(num)
     else:
-        impar = num
+        impares.append(num)
+
 print(f'Os números digitados foram: {sorted(numeros)}')
-print(f'Os números pares digitados foram: {par}')
-print(f'Os números ímpares digitados foram: {impar}')
+print(f'Os números pares digitados foram: {pares}')
+print(f'Os números ímpares digitados foram: {impares}')
